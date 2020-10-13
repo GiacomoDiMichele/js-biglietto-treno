931680163bfef9abd819add08ad810e245171433
Giacomo <giacomo.1936@gmail.com>
2020-10-13 15:33:02 +0200

    impostate domande chilometri/età

---
+++ main.js
@@ -0,0 +1,8 @@
+
+//chiedo quanti chilometri percorrerà il cliente
+var chilometri = prompt('inserisci quanti chilometri percorrerai')
+console.log (chilometri);
+
+//chiedo quanti anni ha il cliente
+var età = prompt('inserisci qui la tua età')
+console.log(età);
