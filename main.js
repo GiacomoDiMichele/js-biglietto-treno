8cec5255ceaa66dc82b1fe3c2a89364bd054a2c8
Giacomo <giacomo.1936@gmail.com>
2020-10-13 15:57:12 +0200

    aggiunto schema algoritmo

--- main.js
+++ main.js
@@ -4,5 +4,15 @@ var chilometri = prompt('inserisci quanti chilometri percorrerai')
 console.log (chilometri);
 
 //chiedo quanti anni ha il cliente
-var età = prompt('inserisci qui la tua età')
+var età = parseInt(prompt('inserisci qui la tua età'))
 console.log(età);
+
+if (età > ) {
+
+}
+else if (età >= ) {
+
+}
+else (età < ) {
+
+}
